7a95c49d414b11202c9f6c50581f85a84a0c6cfe
Viviana Vercesi <vivianavercesi22@gmail.com>
2025-12-08 10:50:02 -0300

    feat: agregar vistar

--- src/app.js
+++ src/app.js
@@ -5,24 +5,44 @@ import { connectDB } from "./config/database.js";
 import userRoutes from "./routes/userRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename); 
 
 const app = express();
-app.use(express.json());
 
 app.use(cors())
 
 // Middlewares
 app.use(express.json());
 
+// Archivos estáticos 
+app.use(express.static(path.join(__dirname, "public")));
+
 // Conexión a MongoDB
 connectDB();
 
+//Rutas API
 app.use(['/users','/usuarios'],userRoutes);
 app.use(['/products','/productos'],productRoutes);
 
+// Rutas de vistas
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "views/login.html"));
+});
+
+app.get("/register", (req, res) => {
+    res.sendFile(path.join(__dirname, "views/register.html"));
+});
+
+app.get("/products-view", (req, res) => {
+    res.sendFile(path.join(__dirname, "views/products.html"));
+});
+
 app.use((req,res) => {
-    res.status(400).json({error:"Ruta no encontrada"})
+    res.status(404).json({error:"Ruta no encontrada"})
 })
 
 const PORT = 3000;
